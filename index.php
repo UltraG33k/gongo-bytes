8bf50f6769ab6cda225bf3e2aa730c751cc30477
UltraG33k <martin@billebroberg.dk>
2019-03-12 21:46:29 +0100

    Start of the project

---
+++ index.php
@@ -0,0 +1,27 @@
+<?
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width">
+        <title>Gongo Bytes Calculator</title>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
+        <script defer src="https://use.fontawesome.com/releases/v5.6.3/js/all.js" integrity="sha384-EIHISlAOj4zgYieurP0SdoiBYfGJKkgWedPHH4jCzpCXLmzVsw1ouK59MuUtP4a1" crossorigin="anonymous"></script>
+	</head>
+	<body>
+		<section class="container has-text-centered">
+			<br>
+			<h1 class="title">Gongo Bytes!</h1>
+			<h2 class="subtitle">A calculator that will convert regular numbers to Gongo Bytes</h2>
+		</section>
+		<section class="container">
+			<div class="field">
+				<div class="control">
+					<input class="input is-large" type="number" placeholder="Input number">
+				</div>
+			</div>
+		</section>
+	</body>
+</html>
